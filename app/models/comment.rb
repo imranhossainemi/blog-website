205aa8ec0fdde6b600688deef16c3b6235e5affb
Imran Hossain <blur-imran@hotmail.com>
2023-06-17 14:03:04 +0600

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end

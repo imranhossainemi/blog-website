94fe7d550d3078d7a05a8a396ec701d551844ec3
Imran Hossain <blur-imran@hotmail.com>
2023-06-17 14:23:20 +0600

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
+  after_create :update_likes_counter
+
   def update_likes_counter
     post.increment!(:likes_counter)
   end

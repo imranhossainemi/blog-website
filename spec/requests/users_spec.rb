ac467973d3963bb5bc94772e2d6d1c17b925f48f
Imran Hossain <blur-imran@hotmail.com>
2023-06-21 14:12:39 +0600

    Update users_spec.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET Users' do
+    before(:each) do
+      get '/users'
+    end
+
+    it 'http_status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Text of a page rendered' do
+      expect(response.body).to include('Welcome')
+    end
+
+    it 'Response status' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'Index template render' do
+      expect(response).to render_template('index')
+    end
+  end
+end
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET Users/ID' do
+    before(:each) do
+      get '/users/1'
+    end
+
+    it 'Show http_status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Show page text' do
+      expect(response.body).to include('Single')
+    end
+
+    it 'Show Response status' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'show template' do
+      expect(response).to render_template('show')
+    end
   end
 end

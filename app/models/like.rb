5d030f3fef48737dcc36ff124cfd3819011839fe
Imran Hossain <blur-imran@hotmail.com>
2023-06-23 14:11:40 +0600

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,11 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
-  belongs_to :post
-
-  after_create :update_likes_counter
-
-  def update_likes_counter
-    post.increment!(:likes_counter)
+  def create
+    post = Post.find(params[:id])
+    @like = post.likes.new(author: current_user)
+    if @like.save
+      redirect_to likes_path(current_user, post)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end

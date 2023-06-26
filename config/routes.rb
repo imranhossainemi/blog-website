89fe461ad92161c8190d1c9f942982a3c33991d2
Imran Hossain <blur-imran@hotmail.com>
2023-06-26 19:46:32 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   get "/users/:id/posts", to: "posts#index", as: 'posts'
   get "/users/:id/posts/new", to: "posts#new", as: 'new_post'
   post "/users/:id/posts", to: "posts#create" 
-  get "/users/:id/posts/:id", to: "posts#show", as: 'post'
+  get "/users/:user_id/posts/:id", to: "posts#show", as: 'user_post'
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show", as: 'user'
   get "/users/:id/posts/:id/comments/new", to: "comments#new", as: 'new_comment'
   post "/users/:id/posts/:id/comments", to: "comments#create", as: 'comments'
   post "/users/:id/posts/:id", to: "likes#create", as: 'likes'
-    
 end

8023aebea95374da1917fba6f09ec1d120b87cd9
Imran Hossain <blur-imran@hotmail.com>
2023-06-26 19:46:06 +0600

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,33 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:id])
-    @posts = @user.posts
+    @user = User.includes(post: [:author, { comments: [:author] }]).find(params[:id])
+    @pagy, @posts = pagy(@user.posts, items: 4)
+    @current_user = current_user
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    @post.author_id = current_user.id
+    if @post.save
+      redirect_to posts_path, notice: 'Post Created Successfully'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end

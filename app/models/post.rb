f02af11d3c53f253e48f69f91b1ba83885d5eb81
Imran Hossain <blur-imran@hotmail.com>
2023-06-28 00:11:01 +0600

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,20 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments
+  has_many :likes
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_create :update_post_counter
+
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
   def index
     @user = User.find(params[:id])
     @posts = @user.posts

a3ede2f100a5fb550b9f6714e502b671b756b706
Imran Hossain <blur-imran@hotmail.com>
2023-06-30 13:45:08 +0600

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,29 @@
+require 'cancancan'
+
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+
   def new
     @comment = Comment.new
   end
 
   def create
-    puts 'Imran'
     @post = Post.find(params[:id])
     @comment = @post.comments.new(author: current_user, **comment_params)
     puts @comment
     puts comment_params
     puts @post
     if @comment.save
-      redirect_to posts_path(@post.author, @post), notice: 'Comment Created Successfully'
+      redirect_to user_post_path(@post.author, @post), notice: 'Comment Created Successfully'
     else
       render :new
     end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_back(fallback_location: root_path)
   end
 
   private

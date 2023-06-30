d1c711ead33686cfd34ae0eeeba8ca5a57c0efe7
Imran Hossain <blur-imran@hotmail.com>
2023-06-30 16:58:50 +0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 

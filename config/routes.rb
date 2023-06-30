81795dd82efd0387613255741bbd616f88058998
Imran Hossain <blur-imran@hotmail.com>
2023-06-30 13:45:35 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
+
   get "/users/:id/posts", to: "posts#index", as: 'posts'
   get "/users/:id/posts/new", to: "posts#new", as: 'new_post'
   post "/users/:id/posts", to: "posts#create" 
@@ -10,4 +11,6 @@ Rails.application.routes.draw do
   get "/users/:id/posts/:id/comments/new", to: "comments#new", as: 'new_comment'
   post "/users/:id/posts/:id/comments", to: "comments#create", as: 'comments'
   post "/users/:id/posts/:id", to: "likes#create", as: 'likes'
+  delete "/users/:user_id/posts/:id", to: "posts#destroy"
+  delete "/users/:user_id/posts/:id/comments/:id", to: "comments#destroy"
 end

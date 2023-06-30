8a91de495e12b9d67275a732b4f0a68c6a2e7fe4
Imran Hossain <blur-imran@hotmail.com>
2023-06-30 19:01:20 +0600

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module BlogWebsite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.api_only = true
 
     # Configuration for the application, engines, and railties goes here.
     #

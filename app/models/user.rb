24d4ef3ad99e8f46d60aff823cd76f04d9f0f08e
Imran Hossain <blur-imran@hotmail.com>
2023-06-30 16:58:52 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  # include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  #  :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :posts, class_name: 'Post', foreign_key: :author_id, dependent: :destroy
   has_many :comments, class_name: 'Comment', foreign_key: :author_id, dependent: :destroy

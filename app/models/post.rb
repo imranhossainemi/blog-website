3d8e1dc9c884eea2516b1e9f780419bc1c25acd0
Imran Hossain <blur-imran@hotmail.com>
2023-06-30 13:45:23 +0600

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -14,37 +14,5 @@ class Post < ApplicationRecord
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
-  end
-
-  def index
-    @user = User.find(params[:id])
-    @posts = @user.posts
-    @current_user = current_user
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.comments_counter = 0
-    @post.likes_counter = 0
-    @post.author_id = current_user.id
-    if @post.save
-      redirect_to posts_path, notice: 'Post Created Successfully'
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :text)
   end
 end

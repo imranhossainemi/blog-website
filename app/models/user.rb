90017a8c22c91cad736665df919ee3f415198d36
Oluchi <okparabon@gmail.com>
2023-06-30 11:14:56 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :posts, class_name: 'Post', foreign_key: :author_id, dependent: :destroy
   has_many :comments, class_name: 'Comment', foreign_key: :author_id, dependent: :destroy

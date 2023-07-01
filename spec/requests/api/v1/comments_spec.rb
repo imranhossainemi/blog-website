6d132669e92b00229cf240e7a426f540c352804c
Imran Hossain <blur-imran@hotmail.com>
2023-07-01 19:54:20 +0600

    Update comments_spec.rb

--- spec/requests/api/v1/comments_spec.rb
+++ spec/requests/api/v1/comments_spec.rb
@@ -1,38 +1,53 @@
-require 'rails_helper'
+# spec/requests/blogs_spec.rb
+require 'swagger_helper'
 
-RSpec.describe 'Api::V1::Comments', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/api/v1/comments/index'
-      expect(response).to have_http_status(:success)
+RSpec.describe 'api/comment', type: :request do
+  path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
+    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
+    parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
+
+    get('list comments') do
+      response(200, 'successful') do
+        let(:user_id) { '123' }
+        let(:post_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/api/v1/comments/show'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
+    # You'll want to customize the parameter types...
+    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
+    parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
 
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/api/v1/comments/create'
-      expect(response).to have_http_status(:success)
-    end
-  end
+    post 'Creates a comments' do
+      tags 'Comments'
+      consumes 'application/json', 'application/xml'
+      parameter name: :comments, in: :body, schema: {
+        type: :object,
+        properties: {
+          text: { type: :string }
+        },
+        required: ['text']
+      }
 
-  describe 'GET /update' do
-    it 'returns http success' do
-      get '/api/v1/comments/update'
-      expect(response).to have_http_status(:success)
-    end
-  end
+      response '201', 'Comments created' do
+        let(:comments) { { text: 'Hi there' } }
+        run_test!
+      end
 
-  describe 'GET /destroy' do
-    it 'returns http success' do
-      get '/api/v1/comments/destroy'
-      expect(response).to have_http_status(:success)
+      response '422', 'invalid request' do
+        let(:pet) { { text: 'foo' } }
+        run_test!
+      end
     end
   end
 end
